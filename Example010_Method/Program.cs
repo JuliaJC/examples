4164df06e5bfcfbd85bded61b6e6e35da412eb70
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-15 19:32:56 +0300

    Example010_Method added

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12, 32, 83, 54, 56, 76, 47, 83 };
+
+int n = array.Length;
+int find = 83;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
